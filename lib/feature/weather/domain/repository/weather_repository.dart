e41328b884971a5d1cd4564b9010990f55cb8dff
Illia Prudkyi <ilya.prudkiy20@gmail.com>
2026-06-10 18:28:27 +0200

    feat(weather): extend weather repository and use case

--- lib/feature/weather/domain/repository/weather_repository.dart
+++ lib/feature/weather/domain/repository/weather_repository.dart
@@ -1,10 +1,22 @@
 import 'package:dartz/dartz.dart';
+import 'package:weather_app/feature/weather/data/models/location_model.dart';
+import 'package:weather_app/feature/weather/domain/entity/weather_daily_forecast_entity.dart';
 import 'package:weather_app/feature/weather/domain/entity/weather_entity.dart';
+import 'package:weather_app/feature/weather/domain/entity/weather_hours_entity.dart';
 import '../../../../core/error/failure/failure.dart';
 
 abstract class WeatherRepository {
   Future<Either<Failure, WeatherEntity>> getWeatherByCityName(String city);
 
-  Future<Either<Failure, WeatherEntity>> getWeatherByGeo(
+  Future<Either<Failure, WeatherEntity>> getWeatherByGeo();
+
+  Future<Either<Failure, LocationModel>> getCords();
+
+  Future<Either<Failure, WeatherEntity>> getDefaultCityWeather();
+
+  Future<Either<Failure, WeatherHoursEntity>> getWeatherHours(
+      double lat, double lon);
+
+  Future<Either<Failure, WeatherDailyForecastEntity>> getWeatherDaily(
       double lat, double lon);
 }
